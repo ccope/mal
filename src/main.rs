077cf0b3278d96e7e74ee1555f3ffd606b943a0a
Cam Cope <github@camcope.me>
2021-04-27 17:02:16 -0700

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
-use actix_web::{
-    App,
-    Error,
-    HttpServer,
-    HttpResponse,
-    Responder,
-    error,
-    web, 
-};
 use actix_session::{CookieSession, Session};
 use actix_web::http::header;
-//use anyhow::Result;
 use actix_web::http::header::ContentType;
+use actix_web::{error, web, App, Error, HttpResponse, HttpServer, Responder, Result};
 use chrono::prelude::*;
 use chrono::Duration;
 use dotenv::dotenv;
 use governor::{Quota, RateLimiter};
 use mime::TEXT_HTML_UTF_8;
 use nonzero_ext::nonzero;
+use oauth2::basic::{BasicClient, BasicTokenType};
+use oauth2::reqwest::async_http_client;
 use oauth2::{
-    AuthorizationCode,
     AuthUrl,
+    AuthorizationCode,
     ClientId,
     ClientSecret,
     CsrfToken,
@@ -32,12 +25,7 @@ use oauth2::{
     TokenResponse,
     TokenUrl,
 };
-use oauth2::basic::{BasicClient, BasicTokenType};
-use oauth2::reqwest::async_http_client;
-use serde::{
-    Deserialize,
-    Serialize,
-};
+use serde::Deserialize;
 use std::env;
 use std::fs::File;
 use tracing::{error, event, instrument, Level};
@@ -72,25 +60,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Create an OAuth2 client by specifying the client ID, client secret, authorization URL and
     // token URL.
-    let client = Box::new(BasicClient::new
-            (
-                client_id,
-                client_secret,
-                auth_url,
-                token_url
-        )
+    let client = Box::new(
+        BasicClient::new(client_id, client_secret, auth_url, token_url),
         // Set the URL the user will be redirected to after the authorization process.
         // .set_redirect_url(redirect_url)
     );
 
     HttpServer::new(move || {
         App::new()
-            .data(AppState { oauth_client: client.clone()})
-            .wrap(CookieSession::signed(&[0; 32])
-                .domain("mal.camcope.me")
-                .name("mal")
-                .secure(false)
-                )
+            .data(AppState {
+                oauth_client: client.clone(),
+            })
+            .wrap(
+                CookieSession::signed(&[0; 32])
+                    .domain("mal.camcope.me")
+                    .name("mal")
+                    .secure(false),
+            )
             .route("/", web::get().to(index))
             .route("/login", web::get().to(login))
             .route("/logout", web::get().to(logout))
@@ -147,7 +133,10 @@ async fn index(session: Session) -> Result<HttpResponse, Error> {
 }
 
 #[instrument(skip(session))]
-async fn get_live_token(session: Session, data: web::Data<AppState>) -> Result<StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>> {
+async fn get_live_token(
+    session: Session,
+    data: web::Data<AppState>,
+) -> Result<StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>> {
     let token = session
         .get::<StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>>("token")
         .unwrap_or(None);
@@ -195,20 +184,22 @@ async fn refresh_token(
 }
 
 #[instrument(skip(session))]
-async fn login(
-    session: Session,
-    data: web::Data<AppState>,
-    ) -> impl Responder {
+async fn login(session: Session, data: web::Data<AppState>) -> impl Responder {
     // Create a PKCE code verifier and SHA-256 encode it as a code challenge.
     // Generate a PKCE challenge.
     let (pkce_challenge, _pkce_verifier) = PkceCodeChallenge::new_random_plain();
     // TODO HAX HAX HAX XXX Uses "plain" pkce challenge type, where it just resends the original
     // code. Switch to sha256 when MAL supports it
     session.insert("PKCE", pkce_challenge.as_str()).unwrap();
-    event!(Level::DEBUG, "\n\nPKCE verifier is {}\n", session.get::<String>("PKCE").unwrap().unwrap());
+    event!(
+        Level::DEBUG,
+        "\n\nPKCE verifier is {}\n",
+        session.get::<String>("PKCE").unwrap().unwrap()
+    );
 
     // Generate the full auth URL to which we'll redirect the user.
-    let (auth_url, csrf_token) = &data.oauth_client
+    let (auth_url, csrf_token) = &data
+        .oauth_client
         .authorize_url(CsrfToken::new_random)
         // Set the desired scopes.
         //.add_scope(Scope::new("read".to_string()))
@@ -225,13 +216,12 @@ async fn login(
         .finish()
 }
 
-    // Once the user has been redirected to the redirect URL, you'll have access to the
-    // authorization code. For security reasons, your code should verify that the `state`
-    // parameter returned by the server matches `csrf_state`.
+// Once the user has been redirected to the redirect URL, you'll have access to the
+// authorization code. For security reasons, your code should verify that the `state`
+// parameter returned by the server matches `csrf_state`.
 
-    // TODO: store csrf_token somehow to verify later
-    // Redis? Cookies?
-
+// TODO: store csrf_token somehow to verify later
+// Redis? Cookies?
 
 #[instrument(skip(session))]
 async fn logout(session: Session) -> HttpResponse {
@@ -252,8 +242,11 @@ async fn auth(
     let state = CsrfToken::new(params.state.clone());
     let _scope = params.scope.clone();
 
-    let verifier = session.get::<PkceCodeVerifier>("PKCE")?.ok_or_else(|| error::ErrorBadRequest("No PKCE verifier found"))?;
-    let token_req = data.oauth_client
+    let verifier = session
+        .get::<PkceCodeVerifier>("PKCE")?
+        .ok_or_else(|| error::ErrorBadRequest("No PKCE verifier found"))?;
+    let token_req = data
+        .oauth_client
         .exchange_code(code)
         .set_pkce_verifier(verifier);
 
@@ -300,7 +293,8 @@ async fn mylist(
     event!(Level::INFO, "entered mylist route");
     let token = get_live_token(session, data).await?;
     let client = reqwest::Client::new();
-    let res = client.get(&(MAL_API.to_string() + "/users/@me/animelist"))
+    let res = client
+        .get(&(MAL_API.to_string() + "/users/@me/animelist"))
         .query(&[("limit", "1000")])
         .bearer_auth(token.access_token().secret())
         .send()
@@ -314,11 +308,13 @@ async fn mylist(
         return Err(error::ErrorInternalServerError(format!("{:?}", res)));
     }
     event!(Level::INFO, "successful MAL fetch");
-    let res_obj: MyAnimeListResponse = res.json()
+    let res_obj: MyAnimeListResponse = res
+        .json()
         .await
         .map_err(|e| error::ErrorInternalServerError(e.to_string()))?;
     let anime: Vec<AnimeListEntry> = res_obj.data.into_iter().map(|x| x.node).collect();
-    serde_json::to_writer(&File::create("./data/animelist.json")?, &anime).map_err(|e| error::ErrorInternalServerError(e.to_string()))?;
+    serde_json::to_writer(&File::create("./data/animelist.json")?, &anime)
+        .map_err(|e| error::ErrorInternalServerError(e.to_string()))?;
     Ok(web::Json(anime))
 }
 
@@ -327,12 +323,15 @@ async fn update_list(
     data: web::Data<AppState>,
 ) -> Result<web::Json<Vec<AnimeListEntry>>, Error> {
     let token = get_live_token(session, data).await?;
-    let mut anime: Vec<AnimeListEntry> = serde_json::from_reader(&File::open("./data/animelist.json")?).map_err(|e| error::ErrorInternalServerError(e.to_string()))?;
+    let mut anime: Vec<AnimeListEntry> =
+        serde_json::from_reader(&File::open("./data/animelist.json")?)
+            .map_err(|e| error::ErrorInternalServerError(e.to_string()))?;
     let client = reqwest::Client::new();
     let lim = RateLimiter::direct(Quota::per_second(nonzero!(2u32)));
     for a in anime.iter_mut() {
         lim.until_ready().await;
-        let res = client.get(&(MAL_API.to_string() + "/anime/" + &a.id.to_string()))
+        let res = client
+            .get(&(MAL_API.to_string() + "/anime/" + &a.id.to_string()))
             .query(&[("fields", "alternative_titles")])
             .bearer_auth(token.access_token().secret())
             .send()
@@ -345,11 +344,13 @@ async fn update_list(
         if res.status() != 200 {
             return Err(error::ErrorInternalServerError(format!("{:?}", res)));
         }
-        let res_obj: MALAltTitleResponse = res.json()
+        let res_obj: MALAltTitleResponse = res
+            .json()
             .await
             .map_err(|e| error::ErrorInternalServerError(e.to_string()))?;
         a.english_title = res_obj.alternative_titles.en;
     }
-    serde_json::to_writer(&File::create("./data/animelist_titled.json")?, &anime).map_err(|e| error::ErrorInternalServerError(e.to_string()))?;
+    serde_json::to_writer(&File::create("./data/animelist_titled.json")?, &anime)
+        .map_err(|e| error::ErrorInternalServerError(e.to_string()))?;
     Ok(web::Json(anime))
 }
